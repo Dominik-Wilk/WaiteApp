36f57edcafb363c7d75aabb2db5c9be31324e399
Dominik Wilk <mr.wilkd@gmail.com>
2023-11-03 22:48:11 +0000

    Add information to be displayed while waiting for a response from the API server

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import RenderTable from '../features/RenderTable';
+import { useSelector } from 'react-redux';
+import { getTables } from '../../redux/tableRedux';
 
-const Home = () => (
-  <>
-    <h1>All tables</h1>
-    <RenderTable />
-  </>
-);
+const Home = () => {
+  const table = useSelector(getTables);
+  return (
+    <>
+      <h1>All tables</h1>
+      {table.length === 0 ? <h2 id='ApiInfo'>Loading...</h2> : <RenderTable />}
+    </>
+  );
+};
 
 export default Home;
